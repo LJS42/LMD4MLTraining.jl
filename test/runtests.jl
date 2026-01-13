7ec3c0995c23d25c0db3b46e78011da0487eb9bd
Despoina Sfantou <despoina.sfantou@ibp.fraunhofer.de>
2026-01-13 15:49:14 +0100

    add/tests for distance, updatesize and normtest

--- test/runtests.jl
+++ test/runtests.jl
@@ -71,3 +71,79 @@ end
     LMD4MLTraining.Train!(learner, 1, false)
     @test true
 end
+
+# All following tests need to be tested!
+# DistanceQuantity
+@testset "DistanceQuantity" begin
+    q = DistanceQuantity()
+    @test q isa LMD4MLTraining.AbstractQuantity
+    @test q.key == :distance
+    @test LMD4MLTraining.distance_key(q) == :distance
+
+    struct DummyParams
+        pa
+        p0
+    end
+
+    pa = (Float32[1, 2, 3], Float32[4, 5])
+    p0 = (Float32[1, 1, 1], Float32[1, 1])
+    params = DummyParams(pa, p0)
+
+    diffs = (pa[1] .- p0[1], pa[2] .- p0[2])
+    expected = sqrt(sum(abs2, diffs[1]) + sum(abs2, diffs[2]))
+
+    r = LMD4MLTraining.compute!(q, nothing, nothing, nothing, params)
+    @test r0 == 0f0
+end
+
+# UpdateSizeQuantity
+@testset "UpdateSizeQuantity" begin
+    q = UpdateSizeQuantity()
+    @test q isa LMD4MLTraining.AbstractQuantity
+    @test q.key == :updatesize
+
+    struct DummyParams
+        pa
+        pb
+    end
+    
+    pa = (Float32[2, 0], Float32[1, 2, 3])
+    pb = (Float32[1, 0], Float32[1, 1, 1])
+    params = DummyParams(pa, pb)
+
+    diffs = (pa[1] .- pb[1], pa[2] .- pb[2])
+    expected = sqrt(sum(abs2, diffs[1]) + sum(abs2, diffs[2]))
+
+    r = LMD4MLTraining.compute!(q, nothing, nothing, nothing, params)
+    @test isapprox(r, expected; rtol=1e-6, atol=1e-6)
+
+    params0 = DummyParams(pb, pb)
+    r0 = LMD4MLTraining.compute!(q, nothing, nothing, nothing, params0)
+    @test r0 == 0f0
+end
+
+# NormTestQuantity
+@testset "NormTestQuantity" begin
+    q = NormTestQuantity()
+    @test q isa LMD4MLTraining.AbstractQuantity
+    @test q.key == :normtest
+
+    losses = Float32[1, 2, 3]
+    B = length(losses)
+
+    function back(seed)
+        idx = findfirst(!iszero, seed)
+        g = idx == 1 ? Float32[1, 0] :
+            idx == 2 ? Float32[0, 2] :
+                       Float32[3, 0]
+        return (g,)
+    end
+
+    grads = Float32[1, 2]
+
+    sample_norm = Float32[1^2 + 0^2, 0^2 + 2^2, 3^2 + 0^2] 
+    expected = 1/(B*(B-1)) * (sum(sample_norm)/sum(abs2, grads) - B)
+
+    r = LMD4MLTraining.compute!(q, losses, back, grads, nothing)
+    @test isapprox(r, expected; rtol=1e-6, atol=1e-6)
+end
